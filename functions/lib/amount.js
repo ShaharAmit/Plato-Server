7dae424873d14e616722a3e15ba37d9760560ef4
shahar amit <shaharamit@shahars-mbp.home>
2018-04-19 09:58:33 +0300

    dynamic update menu when meal raw material reach to red line

--- functions/lib/amount.js
+++ functions/lib/amount.js
@@ -16,32 +16,41 @@ exports.handler = function (change, context) {
             const before = change.before.data();
             const after = change.after.data();
             if (before && after) {
+                const rest = context.params.rest;
                 const restID = context.params.restID;
                 const rawMaterial = context.params.rawMaterial;
-                const docs = yield fb.getCol('shahar/' + restID + '/WarehouseStock/' + rawMaterial + '/Meals');
-                const bAmn = before.amount;
-                const aAmn = after.amount;
-                const redLine = after.redLine;
+                const docs = yield fb.getCol(rest + '/' + restID + '/WarehouseStock/' + rawMaterial + '/Meals');
+                const bAmn = before.value.amount;
+                const aAmn = after.value.amount;
+                const redLine = after.value.redLine;
                 if (bAmn < aAmn) {
                     docs.forEach(doc => {
                         const docVals = doc.data();
                         const docID = doc.id;
-                        if (!docVals.menu && docVals.importance && docVals.redLine < redLine) {
-                            // Update the population of 'SF'
-                            // const ref = fb.db.doc("SF");
-                            // batch.update(sfRef, {"population": 1000000});
+                        if (!docVals.menu && parseFloat(docVals.redLine) < aAmn) {
+                            const batch = fb.db.batch();
+                            batch.update(fb.db.doc(rest + '/' + restID + '/Meals/' + docID), { displayed: true });
+                            batch.update(fb.db.doc(rest + '/' + restID + '/WarehouseStock/' + rawMaterial + '/Meals/' + docID), { menu: true });
+                            batch.commit().then(function () { console.log(docID + '  back displayed'); });
                         }
                     });
                 }
-                else if (bAmn > aAmn) {
+                else if (bAmn > aAmn && aAmn < parseFloat(redLine)) {
                     docs.forEach(doc => {
                         const docVals = doc.data();
                         const docID = doc.id;
+                        if (parseFloat(docVals.redLine) > aAmn && docVals.importance && docVals.menu) {
+                            const batch = fb.db.batch();
+                            batch.update(fb.db.doc(rest + '/' + restID + '/Meals/' + docID), { displayed: false });
+                            batch.update(fb.db.doc(rest + '/' + restID + '/WarehouseStock/' + rawMaterial + '/Meals/' + docID), { menu: false });
+                            batch.commit().then(function () { console.log(docID + ' is no longer displayed'); });
+                        }
                     });
                 }
             }
         }
         catch (err) {
+            console.error(err);
         }
         return 0;
     });
