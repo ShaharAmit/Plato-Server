fae23ba9d78c5302196ed71f4da7a9298a97f44c
Unknown <danielyosef92@gmail.com>
2018-05-30 23:13:11 +0300

    fixed add meal

--- functions/src/addMeal.ts
+++ functions/src/addMeal.ts
@@ -4,10 +4,26 @@ const fb = new firebase();
 
 exports.handler = async (data, context) => {
     console.log('restId: ', data.restId);
-    fb.db.collection('RestAlfa' + '/' + data.restId + '/Meals/').doc(data.meal.name)
-        .set(data.meal);
-    console.log('add meal is working');
-    fb.db.collection('RestAlfa' + '/' + data.restId + '/Meals/' + 'dishes').doc(data.dish.name)
-        .set(data.dish.name);
-    console.log('add dishes is working');
+    const mealDoc = fb.db.collection('RestAlfa' + '/' + data.restId + '/Meals/').doc(data.meal.name);
+    return new Promise((resolve, reject) => {
+        mealDoc.set(data.meal)
+            .then(x => {
+                console.log('add meal is working');
+                data.dishes.forEach(dish => {
+                    mealDoc.collection('dishes').doc(dish).set({id: dish})
+                        .then(result => console.log('dish added to meal', dish))
+                        .catch(reject);
+                });
+
+                Object.keys(data.rawMaterials).forEach(rawMaterial => {
+                    fb.db.doc(`/RestAlfa/${data.restId}/WarehouseStock/${rawMaterial}/Meals/${data.meal.name}`)
+                        .set(data.rawMaterials[rawMaterial])
+                        .catch(reject);
+                });
+
+                resolve();
+            })
+            .catch(reject);
+    });
+
 };
