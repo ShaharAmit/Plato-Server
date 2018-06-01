65a4465c5469df8f7eb65bdee4e8e6e64c21a5b7
Unknown <danielyosef92@gmail.com>
2018-06-01 08:28:24 +0300

    fixed add meal to work with batches

--- functions/src/addMeal.ts
+++ functions/src/addMeal.ts
@@ -5,25 +5,24 @@ const fb = new firebase();
 exports.handler = async (data, context) => {
     console.log('restId: ', data.restId);
     const mealDoc = fb.db.collection('RestAlfa' + '/' + data.restId + '/Meals/').doc(data.meal.name);
+    const batch = fb.db.batch();
+
     return new Promise((resolve, reject) => {
+
         mealDoc.set(data.meal)
             .then(x => {
                 console.log('add meal is working');
                 data.dishes.forEach(dish => {
-                    mealDoc.collection('dishes').doc(dish).set({id: dish})
-                        .then(result => console.log('dish added to meal', dish))
-                        .catch(reject);
+                    batch.set(mealDoc.collection('dishes').doc(dish), {id: dish})
                 });
 
                 Object.keys(data.rawMaterials).forEach(rawMaterial => {
-                    fb.db.doc(`/RestAlfa/${data.restId}/WarehouseStock/${rawMaterial}/Meals/${data.meal.name}`)
-                        .set(data.rawMaterials[rawMaterial])
-                        .catch(reject);
+                    batch.set(fb.db.doc(`/RestAlfa/${data.restId}/WarehouseStock/${rawMaterial}/Meals/${data.meal.name}`)
+                        , data.rawMaterials[rawMaterial])
                 });
 
-                resolve();
+                batch.commit().then(resolve).catch(reject);
             })
             .catch(reject);
     });
-
 };
