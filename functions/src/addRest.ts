4aa1a5cf5b923587f5bab8b18d760c2b1f5e8557
Unknown <danielyosef92@gmail.com>
2018-06-04 17:20:01 +0300

    addRest: added adding sub menus

--- functions/src/addRest.ts
+++ functions/src/addRest.ts
@@ -5,10 +5,17 @@ const fb = new firebase();
 exports.handler = async (data, context) => {
     const batch = fb.db.batch();
     const workingDays = data.workingDays;
+    const subMenus = {list: data.subMenus};
     delete data.workingDays;
+    delete data.subMenus;
+
+    console.log("Starting Add Rest Function");
 
     return new Promise((resolve, reject) => {
         const restDoc = fb.db.collection('RestAlfa').doc(data.id);
+        const subMenusDoc = restDoc.collection('restGlobals').doc('subMenus');
+        console.log("Adding sub menus");
+        batch.set(subMenusDoc, subMenus);
         batch.set(restDoc, data);
         for (let i = 0; i < workingDays.length; i++) {
             batch.set(restDoc.collection('WorkingDays').doc(`${i}`), workingDays[i]);
