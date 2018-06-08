d2e5aa869c5440abb4bf1a46da5753ed0b8302d8
Unknown <danielyosef92@gmail.com>
2018-06-08 13:45:23 +0300

    added adding table

--- functions/src/addTable.ts
+++ functions/src/addTable.ts
@@ -6,6 +6,8 @@ exports.handler = async (data, context) => {
     const batch = fb.db.batch();
 
     return new Promise((resolve, reject) => {
+        batch.set(fb.db.collection(`/RestAlfa/${data.restId}/Tables`).doc(data.table.id), data.table);
+        batch.set(fb.db.collection(`/RestAlfa/${data.restId}/TablesOrders`).doc(data.table.id), {});
         batch.commit().then(resolve).catch(reject);
     });
 };
